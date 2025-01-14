033834f25ea421944185634f7bef2ad3fee977ad
Greg <github@autric.net>
2025-01-14 22:23:03 +0100

    add cache

--- ipinfo-config.js
+++ ipinfo-config.js
@@ -1,5 +1,5 @@
 
-const { IPinfoWrapper } = require("node-ipinfo");
+const { IPinfoWrapper, LruCache } = require("node-ipinfo");
 
 
 module.exports = function(RED) {
@@ -7,8 +7,23 @@ module.exports = function(RED) {
         RED.nodes.createNode(this,n);
         this.name = n.name;
         this.timeout = n.timeout;
+        this.cacheEnabled = n.cacheEnabled;
+        this.maxitem = n.maxitem;
+        this.ttl = n.ttl;
+
         var token = this.credentials.token;
-        this.client = new IPinfoWrapper(token, undefined, this.timeout);        
+        var cache = undefined
+        
+        if(n.cacheEnabled){
+            const cacheOptions = {
+                max: parseInt(this.maxitem),  // 5000
+                ttl: parseInt(this.ttl), // 24 * 1000 * 60 * 60
+            };
+            //console.log("Cache enabled with options: ", JSON.stringify(cacheOptions));
+            cache = new LruCache(cacheOptions);
+        }
+
+        this.client = new IPinfoWrapper(token, cache, this.timeout);        
     }
 
     RED.nodes.registerType("ipinfo-config",IPInfoConfigNode,{
